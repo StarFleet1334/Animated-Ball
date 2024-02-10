955f84de55ba9296bbe8967d8c6eb6fb5007bdf2
IliaLataria01 <latariailia6@gmail.com>
2024-02-10 15:28:53 +0400

    Created Board and ball and rendered them on terminal

--- ball_animation.go
+++ ball_animation.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	const (
+		width  = 50
+		height = 10
+
+		cellEmpty = ' '
+		ball      = '⚾'
+	)
+
+	var cell rune
+
+	board := make([][]bool, width)
+	for row := range board {
+		board[row] = make([]bool, height)
+	}
+
+	board[0][0] = true
+
+	for y := range board[0] {
+		for x := range board {
+			cell = cellEmpty
+			if board[x][y] {
+				cell = ball
+			}
+			fmt.Print(string(cell))
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
 }
